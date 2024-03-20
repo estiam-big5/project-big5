e3c562af066c3338949e5606ed727b998a563d94
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2024-03-20 16:29:16 +0100

    basic navbar component

---
+++ next-app/app/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import NavBarItem from "./NavBarItem";
+
+const menuItems = [
+  {
+    pageTitle : "Home",
+    pageRoute: "/",
+    pageIcon: "/nav/Home.png"
+  },
+  {
+    pageTitle : "Convert",
+    pageRoute: "/convert",
+    pageIcon: "/nav/Convert.png"
+  },
+  {
+    pageTitle : "Wallet",
+    pageRoute: "/wallet",
+    pageIcon: "/nav/Wallet.png"
+  }
+]
+
+export default function NavBar () {
+  let navBarItems = []
+  for (let i=0; i<menuItems.length; i++) {
+    navBarItems.push(<NavBarItem pageTitle={menuItems[i].pageTitle} pageRoute={menuItems[i].pageRoute} pageIcon={menuItems[i].pageIcon} key={`NavBarItem${i}`}/>)
+  }
+  return (
+    navBarItems
+  )
+}
