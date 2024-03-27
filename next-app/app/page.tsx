f4653e444a2060667f931f75e8d54a0c1cff93b6
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2024-03-27 12:53:12 +0100

    fixed the fetching

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -9,6 +9,5 @@ export default function Home() {
       <CryptoStat/>
     </main>
     <NavBar/>
-    
   </>);
 }
--- next-app/app/wallet/page.tsx
+++ next-app/app/wallet/page.tsx
@@ -74,7 +74,7 @@ export default function Wallet() {
           </div>
         )
       }
-      <NavBar />
+      <NavBar/>
     </div>
   );
 }
